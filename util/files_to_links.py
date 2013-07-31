de8d2ecd83adf62d18c1bc081572a62bc17cb54b
Lindsay Young <lyoung@sunlightfoundation.com>
2013-07-31 10:12:05 -0400

    fixed some small mistakes

--- util/files_to_links.py
+++ util/files_to_links.py
@@ -18,20 +18,25 @@ for d in all_docs:
 	file_name = d.url
 	if file_name[:4] != "http":
 		doc_id = str(d.id)
-		stamp_date = d.stamp_date
+		#stamp_date = d.stamp_date
 		reg_id = str(d.reg_id)
-		print file_name
+		file_name = str(file_name)
+
+		stamp_date = re.findall(r'\d{8}', file_name)
+		for stamp in stamp_date:
+			stamp_date = stamp[:4] + "-" + stamp[4:6]
+			doc_dict[reg_id, stamp_date] = doc_id
 		
 
-		#Second round- stamp date
-		if stamp_date != None:
-			stamp_date = stamp_date.strftime("%Y-%m")
-			# if doc_dict.has_key(reg_id):
-			# 	doc_dict[reg_id].append([stamp_date, doc_id])
-			# else:
-			doc_dict[reg_id, stamp_date] = doc_id
-		else:	
-			stupid_list.append([reg_id, doc_id, file_name])
+		# #Second round- stamp date
+		# if stamp_date != None:
+		# 	stamp_date = stamp_date.strftime("%Y-%m")
+		# 	# if doc_dict.has_key(reg_id):
+		# 	# 	doc_dict[reg_id].append([stamp_date, doc_id])
+		# 	# else:
+		# 	doc_dict[reg_id, stamp_date] = doc_id
+		# else:	
+		# 	stupid_list.append([reg_id, doc_id, file_name])
 
 		# First round had the supplemental end in file name, remaining have stamp date
 		# if stamp_date != None:
@@ -46,7 +51,7 @@ for d in all_docs:
 
 print doc_dict
 print stupid_list
-exit()
+
 def add_element(element):
 	try:
 		objects = element.objects.filter(link=file_name)
@@ -54,85 +59,112 @@ def add_element(element):
 			print p.link
 			p.link = url
 			print p.link
-			p.save()
+			#p.save()
 			print "FIXED ", element
 	except:
 		print "pass"
 
-# this changes it is just the search results for the period I am looking at
-url = "https://efile.fara.gov/pls/apex/f?p=125:20:5285552019544927::NO:RP::&cs=3A8FB8A611EFC05ECC57393808F3C577B"
-page = urllib2.urlopen(url).read()
-page = BeautifulSoup(page)
-filings = page.find("table", {"class" : "t14Standard"})
+def doj_files():
+	# this changes it is just the search results for the period I am looking at
+	url = "https://efile.fara.gov/pls/apex/f?p=125:20:5285552019544927::NO:RP::&cs=3A8FB8A611EFC05ECC57393808F3C577B"
+	page = urllib2.urlopen(url).read()
+	page = BeautifulSoup(page)
+	filings = page.find("table", {"class" : "t14Standard"})
 
-for l in filings.find_all('tr'):
-	row = str(l)
-	url = str(re.findall(r'href="(.*?)"', row))
-	url = re.sub("\['",'', re.sub("'\]", '', url))
-	link = str(url)
-	if url[:4] == "http":
-		#print url
-		info = re.findall( r'-(.*?)-', url)
-		reg_id = re.sub('-','', url[25:29])
-		reg_id = re.sub('S','', reg_id)
-		reg_id = str(reg_id)
-	
-		# end_date = l.find_all('td',{"headers" : "SUPPLEMENTALEND DATE"})
-		# end_date = str(end_date)[-16:-6]
-		# end_date_obj = datetime.datetime.strptime(end_date, "%m/%d/%Y")
-		# end_date = end_date_obj.strftime("%Y-%m-%d")
-
-		stamp_date = l.find_all('td',{"headers" : "SUPPLEMENTALEND DATE"})
-		stamp_date = str(stamp_date)[-16:-6]
-		print stamp_date
+	for l in filings.find_all('tr'):
+		row = str(l)
+		url = str(re.findall(r'href="(.*?)"', row))
+		url = re.sub("\['",'', re.sub("'\]", '', url))
+		link = str(url)
+		if url[:4] == "http":
+			#print url
+			info = re.findall( r'-(.*?)-', url)
+			reg_id = re.sub('-','', url[25:29])
+			reg_id = re.sub('S','', reg_id)
+			reg_id = str(reg_id)
 		
-		try:	
-			stamp_date_obj = datetime.datetime.strptime(stamp_date, "%m/%d/%Y")
-			stamp_date = stamp_date_obj.strftime("%Y-%m")
-		except:
-			raw_date = info[1]
-			stamp_date_obj = datetime.datetime.strptime(raw_date, "%Y%m%d")
-			stamp_date = stamp_date_obj.strftime("%Y-%m")
+			# end_date = l.find_all('td',{"headers" : "SUPPLEMENTALEND DATE"})
+			# end_date = str(end_date)[-16:-6]
+			# end_date_obj = datetime.datetime.strptime(end_date, "%m/%d/%Y")
+			# end_date = end_date_obj.strftime("%Y-%m-%d")
+
+			stamp_date = l.find_all('td',{"headers" : "SUPPLEMENTALEND DATE"})
+			stamp_date = str(stamp_date)[-16:-6]
+			print stamp_date
+			
+			try:	
+				stamp_date_obj = datetime.datetime.strptime(stamp_date, "%m/%d/%Y")
+				stamp_date = stamp_date_obj.strftime("%Y-%m")
+			except:
+				raw_date = info[1]
+				stamp_date_obj = datetime.datetime.strptime(raw_date, "%Y%m%d")
+				stamp_date = stamp_date_obj.strftime("%Y-%m")
 
 
-		reg_date = (reg_id, stamp_date)
-		print reg_date
-		#print reg_date
+			reg_date = (reg_id, stamp_date)
+			print reg_date
+			#print reg_date
 
-		if doc_dict.has_key(reg_date):
-			print "Match!"
-			try:
-				doc = Document.objects.get(id=doc_dict[reg_id, stamp_date])
-				file_name = str(doc.url)
+			if doc_dict.has_key(reg_date):
+				print "Match!"
 				try:
-					md = MetaData.objects.get(link=file_name)
-					md.link = url
-					md.save()
-					print "FIXED META DATA"
+					doc = Document.objects.get(id=doc_dict[reg_id, stamp_date])
+					file_name = str(doc.url)
+					try:
+						md = MetaData.objects.get(link=file_name)
+						md.link = url
+						md.processed = True
+						md.notes = "Legacy Data"
+						#md.save()
+						print "FIXED META DATA"
+					except:
+						md = MetaData(link = url,
+							processed = True,
+							notes = "Legacy Data",
+							)
+						md.save()
+
+					add_element(Contact)
+					add_element(Payment)
+					add_element(Disbursement)
+					add_element(Contribution)
+
 				except:
 					pass
 				
+				try:
+					doc = Document.objects.get(id=doc_dict[reg_id, stamp_date])
+					doc.url = url
+					#doc.save()
+					print doc.processed
+					print "Here we go!"
+				except:
+					pass
+					print "NO SAVE", url, stamp_date
 
-				add_element(Contact)
-				add_element(Payment)
-				add_element(Disbursement)
-				add_element(Contribution)
-
-			except:
+			else:
 				pass
-			
-			try:
-				doc = Document.objects.get(id=doc_dict[reg_id, stamp_date])
-				doc.url = url
-				doc.save()
-				print doc.processed
-				print "Here we go!"
-			except:
-				pass
-				print "NO SAVE", url, stamp_date
+				#print "MISS", end_date, url
+#doj_files()
 
+urls = []
+dupes = []
+for d in all_docs:
+	if d.url in urls:
+		print "dupe-- ", d.url, d.processed
+		dupes.append(d.url)
+	else:
+		urls.append(d.url)
+	
+for u in dupes:
+	docs = Document.objects.filter(url=u)
+	for d in docs:
+		if d.processed == True:
+			print "Keep this ", d.url, d.processed
 		else:
-			pass
-			#print "MISS", end_date, url
+			print "Chuck this ", d.url, d.processed
+			#look at the list before deleting 
+			#d.delete()
+
 
  
