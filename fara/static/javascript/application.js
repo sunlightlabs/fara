8cc401dcc888b97abde1f8505e08b4acb11ece4e
Olivia Cheng <olivia@heyitsolivia.com>
2013-10-22 12:26:59 -0400

    Cleaned up homepage, css and js
    Homepage is nicer
    Commented js, put back a missing bracket
    Doclist tables only toggleable with toggle class
    Cleaned up css, and added comments

--- fara/static/javascript/application.js
+++ fara/static/javascript/application.js
@@ -1,39 +1,46 @@
 $(document).ready(function() {
 	
+
+	// Hide toggleable tables on page load depending on setting in cookies
+
 	$.cookie.json = true;
 	var cookie = $.cookie();
-
-	// for (var id in cookie) {
 
 	$('.table_title').each(function(){
 		var e = $(this);
 		
 		if ($.cookie(e.attr('id')) == false) {
-			e.closest('.doclist').find('.table').addClass('js-hidden');
+			e.closest('.doclist.toggle').find('.table').addClass('js-hidden');
 		}
 	});
 
-	$('tbody tr').click(function (){
-		location.href = $(this).find('td a.doclink').attr('href');
-	});          
+	// Toggle toggleable tables on/off and save settings
 
 	$('.table_title').click(function(){
 		var e = $(this);
 		var section_id = e.attr('id')
 
-		var e_table = e.closest('.doclist').find('.table');
+		var e_table = e.closest('.doclist.toggle').find('.table');
 		
 		e_table.toggleClass('js-hidden');
 
 		var displayed = (e_table.hasClass('js-hidden')) ? false : true ;
 
-		$.cookie(section_id, displayed, {
-			expires: 7
-		});
-	
-	console.log($.cookie());
-
+		$.cookie(section_id, displayed, { expires: 7 });
 	});
 
+
+	// Allow user to click on any area in the .table row to access link
+
+	$('tbody tr').click(function (){
+		location.href = $(this).find('td a.doclink').attr('href');
+	});  
+
+
+	// Allow user to click on any area on homepage docselect to access link
+
+	$('.docselect').click(function (){
+		location.href = $(this).find('h3 a').attr('href');
+	});  	        
 
 });
