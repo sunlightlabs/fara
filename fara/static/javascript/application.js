102ac5f9676d315a689467c6889143df1bad3c7e
Olivia Cheng <olivia@heyitsolivia.com>
2013-10-22 15:21:11 -0400

    Adding toggle indicators

--- fara/static/javascript/application.js
+++ fara/static/javascript/application.js
@@ -1,30 +1,37 @@
 $(document).ready(function() {
 	
+	// Insert chevron icon if toggleable
+
+	var chevron = '<span class="glyphicon glyphicon-chevron-down"></span>';
+ 
+	$('.doclist.toggle').each(function() {
+		$(this).find('.table_title').append(chevron);	
+	});
+
 
 	// Hide toggleable tables on page load depending on setting in cookies
 
 	$.cookie.json = true;
 	var cookie = $.cookie();
 
-	$('.table_title').each(function(){
+	$('.table_title').each(function() {
 		var e = $(this);
 		
 		if ($.cookie(e.attr('id')) == false) {
-			e.closest('.doclist.toggle').find('.table').addClass('js-hidden');
+			e.closest('.doclist.toggle').addClass('js-hidden');
 		}
 	});
 
+
 	// Toggle toggleable tables on/off and save settings
 
-	$('.table_title').click(function(){
+	$('.table_title').click(function() {
 		var e = $(this);
-		var section_id = e.attr('id')
+		var id = e.attr('id')
 
-		var e_table = e.closest('.doclist.toggle').find('.table');
-		
-		e_table.toggleClass('js-hidden');
+		var section = e.closest('.doclist.toggle').toggleClass('js-hidden');	
 
-		var displayed = (e_table.hasClass('js-hidden')) ? false : true ;
+		var displayed = (section.hasClass('js-hidden')) ? false : true ;
 
 		$.cookie(section_id, displayed, { expires: 7 });
 	});
@@ -32,14 +39,14 @@ $(document).ready(function() {
 
 	// Allow user to click on any area in the .table row to access link
 
-	$('tbody tr').click(function (){
+	$('tbody tr').click(function() {
 		location.href = $(this).find('td a.doclink').attr('href');
 	});  
 
 
 	// Allow user to click on any area on homepage docselect to access link
 
-	$('.docselect').click(function (){
+	$('.docselect').click(function() {
 		location.href = $(this).find('h3 a').attr('href');
 	});  	        
 
