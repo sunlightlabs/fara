5eafceaca7ebc9df158e0aaa06ba61bd3510c11e
Lindsay Young <lyoung@sunlightfoundation.com>
2013-10-25 16:44:52 -0400

    improve download

--- FaraData/views.py
+++ FaraData/views.py
@@ -36,27 +36,31 @@ def contact_csv(request, form_id):
 	writer = csv.writer(response)
 	writer.writerow(['Date', 'Contact', 'Client', 'Registrant', 'Description', 'Type', 'Employees mentioned', 'Source'])
 	for c in contacts:
+
 		lobbyists = ''
 		for l in c.lobbyist.all():
 			lobbyists = lobbyists + l.lobbyist_name + ", "
 		lobbyists = lobbyists.encode('ascii', errors='ignore')
 		recipients = []
+		contact_name = ''
 		for r in c.recipient.all():
 			contact_name = ''
 			if r.title != None and r.title != '':	
-				contact_name = r.title + ' '
+				contact_name = r.title.encode('ascii', errors='ignore') + ' '
 			if r.name != None and r.name != '':
-				contact_name = contact_name + r.name + ', '
+				contact_name = contact_name + r.name.encode('ascii', errors='ignore') + ', '
 			if r.office_detail != None and r.office_detail != '':
-				contact_name = contact_name + "office: " + r.office_detail + ', '
+				contact_name = contact_name + "office: " + r.office_detail.encode('ascii', errors='ignore') + ', '
 			if r. agency != None and r.agency != '':
-				contact_name = contact_name + "agency: " + r.agency
+				contact_name = contact_name + "agency: " + r.agency.encode('ascii', errors='ignore')
 			contact_name = contact_name + "; "
 			contact_name = contact_name.encode('ascii', errors='ignore')
+		
 		if c.date == None:
 			date = dumb_date.strftime('%x') + "*"
 		else:
 			date = c.date
+		
 		c_type = {"M": "meeting", "U":"unknown", "P":"phone", "O": "other", "E": "email"}
 
 		writer.writerow([date, contact_name, c.client, c.registrant, c.description, c_type[c.contact_type], lobbyists, c.link])
