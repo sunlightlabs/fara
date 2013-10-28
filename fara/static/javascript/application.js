92af9f52b5786a16f3cc76bd70a6c8f192d88474
Olivia Cheng <olivia@heyitsolivia.com>
2013-10-28 18:31:31 -0400

    Added improvements to amendments form
    Cleaned HTML
    Cleaned up JS

--- fara/static/javascript/application.js
+++ fara/static/javascript/application.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	
+
 	// Insert chevron icon if toggleable
 
 	var chevron = '<span class="glyphicon glyphicon-chevron-down"></span>';
@@ -25,7 +26,7 @@ $(document).ready(function() {
 
 	// Toggle toggleable tables on/off and save settings to cookie
 
-	$('.table_title').click(function() {
+	$('.table_title').on('click', function() {
 		var e = $(this);
 		var id = e.attr('id')
 
@@ -37,17 +38,60 @@ $(document).ready(function() {
 	});
 
 
-	// Allow user to click on any area in the .table row to access link
 
-	$('.table tbody tr').click(function() {
-		location.href = $(this).find('td a.doclink').attr('href');
+	// Custom popup function for links
+
+	var popup = function(linkClass) {
+		$(document).on('click', linkClass, function(e){
+			e.preventDefault();
+
+			var url = $(this).attr('href');
+			newPopup = window.open(url, 'name', 'width=550, height=750');
+			
+			if (window.focus) {
+				newPopup.focus();
+			}
+			return false;			
+		});
+	}
+
+	var popupClass = 'js-popup'; // Define the popup class
+	var popupSelector = '.' + popupClass; // Define the popup selector
+
+	popup(popupSelector); // Trigger popup on this selector
+
+
+
+	// Allow user to click on any area in the .table row to access a link 
+
+	$(document).on('click', '.table tbody tr', function() {
+
+		if ($('td a', this).hasClass(popupClass)) { // If the link is a popup, create a popup window for it
+
+			var url = $(popupSelector, this).attr('href');
+			newPopup = window.open(url, 'name', 'width=550, height=750');
+			
+			if (window.focus) {
+				newPopup.focus();
+			}
+			return false;	
+
+		} else { // Otherwise redirect normally
+			location.href = $('td .doclink', this).attr('href');
+		}
+
 	});  
 
 
 	// Allow user to click inside .docselect on the homepage to access link
 
-	$('.docselect').click(function() {
+	$('.docselect').on('click', function() {
 		location.href = $(this).find('h3 a').attr('href');
 	});  	        
 
 });
+
+
+
+
+
