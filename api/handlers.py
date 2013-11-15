371cb3172f52247c0daf1a3211fc7844b5288916
Lindsay Young <lyoung@sunlightfoundation.com>
2013-11-15 14:14:36 -0500

    Got Doc API to work

--- api/handlers.py
+++ api/handlers.py
@@ -8,8 +8,8 @@ class DocumentHandler(BaseHandler):
 
 	def read(self, request, doc_id=None):
 		base = Document.objects
-
+		
 		if doc_id:
-			return base.get(pk=doc_id)
+			return base.get(id=doc_id)
 		else:
-			return base.all()
+			return base.all().order_by('-stamp_date')
