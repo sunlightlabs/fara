3f955c1fe4b74d7fa656f583240d58620283db64
Lindsay Young <lyoung@sunlightfoundation.com>
2013-11-18 17:54:26 -0500

    adding metadata and registrant models to API

--- api/handlers.py
+++ api/handlers.py
@@ -4,7 +4,8 @@ from FaraData.models import *
 
 class DocumentHandler(BaseHandler):
 	allowed_methods = ('GET',)
-	model = Document   
+	model = Document
+	fields = ('url', 'reg_id', 'doc_type', 'processed', 'stamp_date')   
 
 	def read(self, request, doc_id=None):
 		base = Document.objects
@@ -17,15 +18,44 @@ class DocumentHandler(BaseHandler):
 class RegDocHandler(BaseHandler):
 	allowed_methods = ('GET',)
 	model = Document
+	fields = ('url', 'reg_id', 'doc_type', 'processed', 'stamp_date')
 
 	def read(self, request, reg_id=None):
 		base = Document.objects
 
 		if reg_id:
 			return base.filter(reg_id=reg_id)
-		else:
-			return base.all().order_by('-stamp_date')
 
+
+class MetaDataHandler(BaseHandler):
+	allowed_methods = ('GET',)
+	model = MetaData
+	# notes are not viewable from the API
+	fields = ( 'processed', 'form', 'link', 'upload_date', 'end_date','reviewed', 'is_amendment')
+
+	def read(self, request, form_id=None):
+		base = MetaData.objects
+		if form_id:
+			return base.filter(form=form_id)
+
+class RegistrantDataHandler(BaseHandler):
+	allowed_methods = ('GET',)
+	model = Registrant
+	fields = ('reg_id', 'reg_name', 'address', 'city', 'state', 'zip_code', 'country', 'terminated_clients', 'clients')
+
+	def read(self, request, reg_id=None):
+		base = Registrant.objects
+		if reg_id:
+			return base.filter(reg_id=reg_id)
+		else:
+			return base.all()
+"""
+For the view page, I need to add look up by source url for:
+			contacts
+			contributions
+			payments
+			disbursements
+"""
 
 #class ContactHandler(BaseHandler):
 	#Contact by Registrant
--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,20 @@
 from django.conf.urls import *
 from piston.resource import Resource
 from piston.authentication import HttpBasicAuthentication
-from handlers import DocumentHandler, RegDocHandler
+from handlers import DocumentHandler, RegDocHandler, MetaDataHandler, RegistrantDataHandler
 
 auth = HttpBasicAuthentication(realm="Narnia")
 document_handler = Resource(DocumentHandler, authentication=auth)
 regdoc_handler = Resource(RegDocHandler, authentication=auth)
+metadata_handler = Resource(MetaDataHandler, authentication=auth)
+registrant_handler = Resource(RegistrantDataHandler, authentication=auth)
 
 urlpatterns = patterns('',
    url(r'^doc/(\d+)/', document_handler),
    url(r'^docs', document_handler),
    url(r'^regdocs/(\d+)/', regdoc_handler),
+   url(r'^metadata/(\d+)/', metadata_handler),
+   url(r'^registrant/(\d+)/', registrant_handler),
+   url(r'^registrants', registrant_handler),
    #url(r'^blogposts/', blogpost_handler),
 )
