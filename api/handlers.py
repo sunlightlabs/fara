71cad6ad5e7cc45f3365eb4e658b4b9d6004d656
Lindsay Young <lyoung@sunlightfoundation.com>
2013-11-18 11:51:32 -0500

    Added an endpoint to look up all documents for a registrant

--- api/handlers.py
+++ api/handlers.py
@@ -1,6 +1,6 @@
 from piston.handler import BaseHandler
 from fara_feed.models import *
-# from FaraData.models import *
+#from FaraData.models import *
 
 class DocumentHandler(BaseHandler):
 	allowed_methods = ('GET',)
@@ -11,5 +11,17 @@ class DocumentHandler(BaseHandler):
 		# document type would make for good kwarg
 		if doc_id:
 			return base.get(id=doc_id)
+		else:
+			return base.all().order_by('-stamp_date')
+
+class RegDocHandler(BaseHandler):
+	allowed_methods = ('GET',)
+	model = Document
+
+	def read(self, request, reg_id=None):
+		base = Document.objects
+
+		if reg_id:
+			return base.filter(reg_id=reg_id)
 		else:
 			return base.all().order_by('-stamp_date')
--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import *
 from piston.resource import Resource
 from piston.authentication import HttpBasicAuthentication
-from handlers import DocumentHandler
+from handlers import DocumentHandler, RegDocHandler
 
 #auth = HttpBasicAuthentication(realm="Narnia")
 document_handler = Resource(DocumentHandler)
+regdoc_handler = Resource(RegDocHandler)
 
 urlpatterns = patterns('',
    url(r'^doc/(\d+)/', document_handler),
    url(r'^docs', document_handler),
+   url(r'^regdocs/(\d+)/', regdoc_handler),
    #url(r'^blogposts/', blogpost_handler),
 )
