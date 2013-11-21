70dd52603515de91edb244c44b38939a3b516ddd
lindsay@fara <lindsay@fara>
2013-11-21 18:37:04 +0000

    settings update

--- fara/settings.py
+++ fara/settings.py
@@ -58,7 +58,6 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -68,7 +67,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static_root')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'https://fara.sunlightfoundation.com.s3.amazonaws.com/'
+#STATIC_URL = 'https://fara.sunlightfoundation.com.s3.amazonaws.com/'
+STATIC_URL = 'http://fara.sunlightfoundation.com.s3.amazonaws.com/'
+
+MEDIA_URL = STATIC_URL
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
