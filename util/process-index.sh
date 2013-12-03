df7d74083d8f07c6d67b227a84ce4bd109280373
lindsay@fara <lindsay@fara>
2013-12-03 16:06:58 +0000

    some cleanup to process-index.sh

--- util/process-index.sh
+++ util/process-index.sh
@@ -9,14 +9,6 @@
 
 html_dir="/mnt/html"
 
-#if [[ ! -d $ROOT_DIR/$1 || -z $1 ]]
-#then
-#	echo "must give me a real directory ... i may not care if there are files in that directory !"
-#	exit 1
-#else
-#	DEST_DIR=$ROOT_DIR/$1
-#fi
-
 if [ $1 ]
 then
 	BASE_NAME=$1
@@ -30,8 +22,8 @@ HTML_DIR=${2:-$html_dir}
 
 DEST_DIR=$HTML_DIR/$BASE_NAME
 
-echo "\$1 == $1"
-echo "\$DEST_DIR == $DEST_DIR"
+#echo "\$1 == $1"
+#echo "\$DEST_DIR == $DEST_DIR"
 
 echo "mv $DEST_DIR/$1.html $DEST_DIR/index.html"
 #mv $DEST_DIR/$1.html $DEST_DIR/index.html
@@ -52,28 +44,3 @@ echo "mv $DEST_DIR/tmp4 $DEST_DIR/index.html"
 
 #rm -f $DEST_DIR/tmp[1234] $DEST_DIR/base.min.css $DEST_DIR/fancy.min.css $DEST_DIR/compatibility.min.js $DEST_DIR/pdf2htmlEX.min.js
 echo "rm -f $DEST_DIR/tmp[1234] $DEST_DIR/base.min.css $DEST_DIR/fancy.min.css $DEST_DIR/compatibility.min.js $DEST_DIR/pdf2htmlEX.min.js"
-
-#
-#for FILE in $WERK_DIR/*.pdf
-#do 
-#	#echo $FILE
-#	BASE=$(basename $FILE .pdf)
-#	echo $BASE
-#	DEST_DIR=$TOP_DEST_DIR/$BASE
-#
-#	# convert using pdf2htmlEX  ## SIMPLE OPTIONS ## 
-#	pdf2htmlEX --embed cfijo --external-hint-tool=ttfautohint --dest-dir $DEST_DIR $FILE
-#
-#	mv $DEST_DIR/$BASE.html $DEST_DIR/index.html
-#	# deal w/ css assets 
-#	cat $DEST_DIR/index.html | sed 's/base.min.css/\/css\/base.min.css/' > $DEST_DIR/tmp1	
-#	cat $DEST_DIR/tmp1 	 | sed 's/fancy.min.css/\/css\/fancy.min.css/' > $DEST_DIR/tmp2
-#
-#	# deal w/ js assets
-#	cat $DEST_DIR/tmp2 | sed 's/compatibility.min.js/\/javascript\/compatibility.min.js/' > $DEST_DIR/tmp3
-#	cat $DEST_DIR/tmp3 | sed 's/pdf2htmlEX.min.js/\/javascript\/pdf2htmlEX.min.js/' > $DEST_DIR/tmp4
-#
-#	mv $DEST_DIR/tmp4 $DEST_DIR/index.html
-#	rm -f $DEST_DIR/tmp[1234] $DEST_DIR/base.min.css $DEST_DIR/fancy.min.css $DEST_DIR/compatibility.min.js $DEST_DIR/pdf2htmlEX.min.js
-#	#exit 3 # break for testing
-#done
