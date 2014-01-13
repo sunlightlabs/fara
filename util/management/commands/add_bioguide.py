ac163d565ae7879ff764b87352a7c6412a0f2275
Lindsay Young <lyoung@sunlightfoundation.com>
2014-01-13 12:52:10 -0500

    spread sheets ids problem registrants

--- util/management/commands/add_bioguide.py
+++ util/management/commands/add_bioguide.py
@@ -16,7 +16,7 @@ class Command(BaseCommand):
 		recipients = Recipient.objects.filter(crp_id__isnull=False)
 		filename = "data/CRP_ID_issues" + str(datetime.date.today()) + ".csv"
 		writer = csv.writer(open(filename, 'wb'))
-		writer.writerow(["crp_id", "title", "name", "agency", "office", "local"])
+		writer.writerow(["id", "crp_id", "title", "name", "agency", "office", "local"])
 
 		for person in recipients:
 			crp = person.crp_id
@@ -30,14 +30,14 @@ class Command(BaseCommand):
 				person.save()
 			else:
 				if crp[:1] != "N":
-					writer.writerow([person.crp_id, person.title, person.name, person.agency, person.office_detail, person.state_local])
+					writer.writerow([person.id, person.crp_id, person.title, person.name, person.agency, person.office_detail, person.state_local])
 				elif crp not in failures: 
 					bio = find_bio(crp)
 					if bio != None:
 						person.bioguide_id = bio
 						person.save()
 					else:
-						writer.writerow([person.crp_id, person.title, person.name, person.agency, person.office_detail, person.state_local])
+						writer.writerow([person.id, person.crp_id, person.title, person.name, person.agency, person.office_detail, person.state_local])
 
 def find_bio(crp):
 	query_params = { 'crp_id': crp,
