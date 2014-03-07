ec04594c0b2207d34ebdfc448a2e7b646a195786
Lindsay Young <lyoung@sunlightfoundation.com>
2014-03-07 17:55:38 -0500

    add an s, I wish i had a local example for this

--- api/views.py
+++ api/views.py
@@ -380,7 +380,7 @@ def reg_profile(request, reg_id):
 		# checking the end date
 		docs = []
 		for doc in doc_list:
-			md = MetaData.object.get(link=doc)
+			md = MetaData.objects.get(link=doc)
 			end_date = md.en_date
 			if datetime.date(2013,1,1) > md.end_date > datetime.date(2013,12,31):
 				docs.append(doc_dict[key])
