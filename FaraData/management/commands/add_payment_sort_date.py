640e9ffcc805c991fdf64a7b0f638023436b4650
Lindsay Young <lyoung@sunlightfoundation.com>
2014-03-15 16:53:34 -0400

    test script to add payment sort date

--- FaraData/management/commands/add_payment_sort_date.py
+++ FaraData/management/commands/add_payment_sort_date.py
@@ -1,16 +1,20 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.core.files.storage import default_storage
 
-from FaraData.models import Payment, MetaData
+from FaraData.models import Payment, MetaData 
+from fara_feed.models import Document
 
 payments = Payment.objects.all()
 
 for payment in payments:
+	md = MetaData.objects.get(link=payment.link)
 	if payment.date is not None:
 		payment.sort_date = payment.date
-	else:
-		md = MetaData.objects.get(link=payment.link)
+	elif md.end_date is not None:
 		payment.sort_date = md.end_date
+	else:
+		doc = Document.objects.get(url=payment.link)
+		payment.sort_date = doc.stamp_date
 	print payment.sort_date
 	# payment.save()
 
