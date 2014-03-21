7d257f2804270b4281120bc2a2732e19484e6200
Lindsay Young <lyoung@sunlightfoundation.com>
2014-03-20 20:49:18 -0400

    headings

--- FaraData/management/commands/create_form_downloads.py
+++ FaraData/management/commands/create_form_downloads.py
@@ -8,6 +8,14 @@ from FaraData import spread_sheets
 from FaraData.models import Contact, Payment, Disbursement, Contribution
 from fara_feed.models import Document
 
+# HEADINGS
+contact_heading = ['Date', 'Contact Title','Contact Name', 'Contact Office', 'Contact Agency', 'Client', 'Client Location', 'Registrant', 'Description', 'Type', 'Employees Mentioned', 'Affiliated Member Bioguide ID', 'Source', 'Document ID', 'Registrant ID', 'Client ID', 'Location ID', 'Recipient ID', 'Record ID']
+contribution_heading = ['Date', 'Amount', 'Recipient', 'Registrant', 'Contributing Individual or PAC', 'CRP ID of Recipient', 'Bioguide ID', 'Source', 'Document ID', 'Registrant ID', 'Recipient ID', 'Record ID']
+payment_heading = ['Date', 'Amount', 'Client', 'Registrant', 'Purpose', 'From subcontractor', 'Source', 'Document ID', 'Registrant ID', 'Client ID','Location ID', 'Subcontractor ID', 'Record ID']
+disbursement_heading = ['Date', 'Amount', 'Client', 'Registrant', 'Purpose', 'To Subcontractor', 'Source', 'Document ID' 'Registrant ID', 'Client ID','Location ID', 'Subcontractor ID', 'Record ID']
+client_reg_heading = ['Client', 'Registrant name', 'Terminated', 'Location of Client', 'Description of service (when available)', 'Registrant ID', 'Client ID', 'Location ID']
+
+
 
 class Command(BaseCommand):
 	help = "Creates one zipfile of spreadsheets for each form to buckets."
