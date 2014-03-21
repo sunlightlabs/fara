3d6ea9954d868cec44ba9564497be3642984b3fa
Lindsay Young <lyoung@sunlightfoundation.com>
2014-03-21 14:00:20 -0400

    ready to save

--- FaraData/management/commands/create_form_downloads.py
+++ FaraData/management/commands/create_form_downloads.py
@@ -13,46 +13,46 @@ class Command(BaseCommand):
 	can_import_settings = True
 
 	def handle(self, *args, **options):
-		print "starting"
-		# # client-registrant
-		# filename = "InfluenceExplorer/client_registrant.csv"
-		# cr_file = default_storage.open(filename, 'wb')
-		# writer = csv.writer(cr_file)
-		# client_registrant(writer)
-		# #cr_file.close()
-		# print "done with client registrant"
+		print "starting", datetime.datetime.now().time()
+		# client-registrant
+		filename = "InfluenceExplorer/client_registrant.csv"
+		cr_file = default_storage.open(filename, 'wb')
+		writer = csv.writer(cr_file)
+		client_registrant(writer)
+		cr_file.close()
+		print "done with client registrant"
 
-		# # disbursements
-		# disbursements = Disbursement.objects.filter(meta_data__processed=True)
-		# filename = "InfluenceExplorer/disbursements.csv"
-		# disbursement_file = default_storage.open(filename, 'wb')
-		# writer = csv.writer(disbursement_file)
-		# writer.writerow(disbursement_heading)
-		# disbursements_sheet(disbursements, writer)
-		# #disbursement_file.close()
-		# print "done with disbursements"
+		# disbursements
+		disbursements = Disbursement.objects.filter(meta_data__processed=True)
+		filename = "InfluenceExplorer/disbursements.csv"
+		disbursement_file = default_storage.open(filename, 'wb')
+		writer = csv.writer(disbursement_file)
+		writer.writerow(disbursement_heading)
+		disbursements_sheet(disbursements, writer)
+		disbursement_file.close()
+		print "done with disbursements"
 
-		# # contributions
-		# contributions = Contribution.objects.filter(meta_data__processed=True)
-		# filename = "InfluenceExplorer/contributions.csv"
-		# contribution_file = default_storage.open(filename, 'wb')
-		# writer = csv.writer(contribution_file)
-		# writer.writerow(contribution_heading)
-		# contributions_sheet(contributions, writer)
-		# contribution_file.close()
-		# print "done with contributions"
+		# contributions
+		contributions = Contribution.objects.filter(meta_data__processed=True)
+		filename = "InfluenceExplorer/contributions.csv"
+		contribution_file = default_storage.open(filename, 'wb')
+		writer = csv.writer(contribution_file)
+		writer.writerow(contribution_heading)
+		contributions_sheet(contributions, writer)
+		contribution_file.close()
+		print "done with contributions"
 
-		# # payments
-		# payments = Payment.objects.filter(meta_data__processed=True)
-		# filename = "InfluenceExplorer/payments.csv"
-		# payment_file = default_storage.open(filename, 'wb')
-		# writer = csv.writer(payment_file)
-		# writer.writerow(payment_heading)
-		# payments_sheet(payments, writer)
-		# #payment_file.close()
-		# print "done with payments"
+		# payments
+		payments = Payment.objects.filter(meta_data__processed=True)
+		filename = "InfluenceExplorer/payments.csv"
+		payment_file = default_storage.open(filename, 'wb')
+		writer = csv.writer(payment_file)
+		writer.writerow(payment_heading)
+		payments_sheet(payments, writer)
+		payment_file.close()
+		print "done with payments"
 		
-		# contacts
+		contacts
 		contacts = Contact.objects.filter(meta_data__processed=True)
 		print "got contacts"
 		filename = "InfluenceExplorer/contacts.csv"
@@ -60,8 +60,10 @@ class Command(BaseCommand):
 		writer = csv.writer(contact_file)
 		writer.writerow(contact_heading)
 		contact_sheet(contacts, writer)
-		#contact_file.close()
+		contact_file.close()
 		print "done with contacts"
+
+		print "ending", datetime.datetime.now().time()
 		
 
 		
