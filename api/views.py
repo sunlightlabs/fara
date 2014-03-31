940704e20974cb0c614b9bfee977716b81cb7fd0
Lindsay Young <lyoung@sunlightfoundation.com>
2014-03-31 17:38:32 -0400

    making sure the api does not fail if no date in arms info

--- api/views.py
+++ api/views.py
@@ -119,13 +119,16 @@ def incoming_arms(request):
 		arms_pool = Proposed.objects.filter(location_id=request.GET.get('location_id'))
 	elif request.GET.get('doc_id'):
 		arms = Proposed.objects.get(id=request.GET.get('doc_id'))
+	
 		if arms.date:
 			try:
-				date = arms.date
+				date = arms.date.strftime("%m/%d/%Y")
 			except:
 				date = None
 		else:
-			date = None
+			print arms, "Has no date"
+			date =  None
+		
 		return HttpResponse(json.dumps({'pdf_url':arms.pdf_url, 'date':date, 'title':arms.title, 'id':arms.id, 'location':arms.location, 'location_id':arms.location_id}), mimetype="application/json")
 	else:
 		arms_pool = Proposed.objects.all().order_by('-date')
@@ -145,11 +148,16 @@ def incoming_arms(request):
 		count += 1
 		record['id'] = arms_press.id
 		record['title'] = arms_press.title
-		if arms_press.date:
+		
+		if arms.date:
 			try:
-				record['date'] = arms_press.date.strftime("%m/%d/%Y")
+				record['date'] = arms.date.strftime("%m/%d/%Y")
 			except:
 				record['date'] = None
+		else:
+			print record, "Has no date"
+			record['date'] =  None
+
 		# this is not scraped yet
 		if arms_press.amount:
 			record['amount'] = arms_press.amount
