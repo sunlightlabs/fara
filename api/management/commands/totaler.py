ae44b49b46e24b4de9a649e2222f5ae21dabeda5
Lindsay Young <lyoung@sunlightfoundation.com>
2014-04-24 15:35:18 -0400

    debug

--- api/management/commands/totaler.py
+++ api/management/commands/totaler.py
@@ -188,8 +188,8 @@ def client_totals(lobbying_regs, docs):
 		total = preliminary_total
 		client_totals[client]['total'] = total
 		location_id = client_totals[client]['location_id']
-		print location_totals[location_id]['total']
-		location_totals = location_totals[location_id]['total'] + total
+		amount = float(location_totals[location_id]['total'])
+		location_totals = amount + total
 
 	with open("api/computations/client13.json", 'w') as f:
 		results = json.dumps(client_totals, separators=(',',':'))
