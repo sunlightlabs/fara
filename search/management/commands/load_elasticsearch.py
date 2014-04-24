c0fa7a940066ee2a6e705b149cef51032203f9ab
Lindsay Young <lyoung@sunlightfoundation.com>
2014-04-24 18:03:22 -0400

    will load all es catagories

--- search/management/commands/load_elasticsearch.py
+++ search/management/commands/load_elasticsearch.py
@@ -20,18 +20,18 @@ class Command(BaseCommand):
 	can_import_settings = True
 
 	def handle(self, *args, **options):
-		# load_clients()
-		# load_locations()
-		# load_registrants()
-		# load_recipients()
-		## load_lobby()
-		# load_arms()
+		load_clients()
+		load_locations()
+		load_registrants()
+		load_recipients()
+		# load_lobby()
+		load_arms()
 		load_contacts()
 		load_payments()
 		load_disbursements()
 		load_contributions()
 		load_gifts()
-		# load_fara_text()
+		load_fara_text()
 
 def load_clients():
 	for client in Client.objects.all():
