89f0bc0628ea9ea0694444223d48e1c1a7ed7250
Lindsay Young <lyoung@sunlightfoundation.com>
2014-05-05 13:39:28 -0400

    breaking out totals

--- cron/crontotals.sh
+++ cron/crontotals.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-
 source $HOME/virt/bin/activate
 
 cd /projects/fara/src/fara
-/projects/fara/virt/bin/python manage.py create_form_downloads > $HOME/cron/create_downloads.log 2>&1
+/projects/fara/virt/bin/python manage.py create_form_downloads --contacts > $HOME/cron/create_downloads.log 2>&1
+/projects/fara/virt/bin/python manage.py create_form_downloads --payments > $HOME/cron/create_downloads.log 2>&1
+/projects/fara/virt/bin/python manage.py create_form_downloads --disbursements > $HOME/cron/create_downloads.log 2>&1
+/projects/fara/virt/bin/python manage.py create_form_downloads --contributions > $HOME/cron/create_downloads.log 2>&1
+/projects/fara/virt/bin/python manage.py create_form_downloads --client_reg > $HOME/cron/create_downloads.log 2>&1
 /projects/fara/virt/bin/python manage.py totaler > $HOME/cron/totaler.log 2>&1
-
