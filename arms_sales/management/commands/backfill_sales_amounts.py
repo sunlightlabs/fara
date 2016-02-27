0a565915165b8bb9ba96b218ed42b25060a87913
Aliya Rahman <aliya.rahman@yahoo.com>
2016-02-26 22:32:07 -0500

    Added command for sales amount backfill

---
+++ arms_sales/management/commands/backfill_sales_amounts.py
@@ -0,0 +1,33 @@
+# This Python file uses the following encoding: utf-8
+# Backfills proposed sale amounts in dollars for documents already in the database
+import requests
+import re
+
+from django.conf import settings 
+from django.core.management.base import BaseCommand, CommandError
+
+from arms_sales.models import Proposed
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        base_url = "http://www.dsca.mil/"
+        proposed_sales = Proposed.objects.all()
+        for p in proposed_sales:
+            if p.amount != None:
+                pass
+            else:
+                try:
+                    price_string = requests.get(p.dsca_url).text.split("$")[1][:100]
+                    raw_amount = re.compile(r'<[^>]+>').sub('', price_string).split("lion")[0]+"lion"
+                    if " million" in raw_amount:
+                        p.amount = float(raw_amount.strip(" million")+'000000')
+                    elif " billion" in raw_amount:
+                        p.amount = float(raw_amount.strip(" billion")+'000000000')
+                    else:
+                        p.amount = float(amount)
+                    p.save()
+                except:
+                    print p.title+": Could not find a dollar amount"
+            print p.amount
+        
--- arms_sales/models.py
+++ arms_sales/models.py
@@ -5,7 +5,7 @@ class Proposed(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField(null=True, blank=True)
     date = models.DateField(null=True, blank=True)
-    amount = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    amount = models.DecimalField(max_digits=11, decimal_places=2, null=True, blank=True)
     location = models.CharField(max_length=200)
     location_id = models.IntegerField(null=True, blank=True)
     # from DSCAmodels.IntegerField(
@@ -20,3 +20,4 @@ class proposed_admin(admin.ModelAdmin):
     search_fields=['title']
 admin.site.register(Proposed, proposed_admin)
 
+180000000
