b9698242bcc1751fef67775fdc8a6763378a5773
Aliya Rahman <aliya.rahman@yahoo.com>
2016-02-27 14:04:03 -0500

    Adjusted logic in backfill command for floats

--- arms_sales/management/commands/backfill_sales_amounts.py
+++ arms_sales/management/commands/backfill_sales_amounts.py
@@ -14,25 +14,25 @@ class Command(BaseCommand):
         base_url = "http://www.dsca.mil/"
         proposed_sales = Proposed.objects.all()
         for p in proposed_sales:
-            #if p.amount != None and p.amount !=115100000.00:
-            #    pass
-            #else:
-            try:
-                url = p.dsca_url
-                if p.amount == 115100000.00:
-                    p.amount = None
+            if p.amount != None and p.amount !=115100000.0:
+                pass
+            else:
+                try:
+                    url = p.dsca_url
+                    if p.amount == 115100000.0:
+                        p.amount = None
+                        p.save()
+                        url = p.pdf_url
+                    price_string = requests.get(url).text.split("$")[1][:100]
+                    raw_amount = re.compile(r'<[^>]+>').sub('', price_string).split("lion")[0]+"lion"
+                    if " million" in raw_amount:
+                        p.amount = float(raw_amount.strip(" million"))*1000000
+                    elif " billion" in raw_amount:
+                        p.amount = float(raw_amount.strip(" billion"))*1000000000
+                    else:
+                        p.amount = float(amount)
                     p.save()
-                    url = p.pdf_url
-                price_string = requests.get(url).text.split("$")[1][:100]
-                raw_amount = re.compile(r'<[^>]+>').sub('', price_string).split("lion")[0]+"lion"
-                if " million" in raw_amount:
-                    p.amount = float(raw_amount.strip(" million"))*1000000
-                elif " billion" in raw_amount:
-                    p.amount = float(raw_amount.strip(" billion"))*1000000000
-                else:
-                    p.amount = float(amount)
-                p.save()
-            except:
-                print p.title+": Could not find a dollar amount"
+                except:
+                    print p.title+": Could not find a dollar amount"
             print str(p.amount) + ": "+p.title[:45]
         
